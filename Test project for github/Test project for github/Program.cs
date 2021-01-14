95602aeaca8ce848497c59a649e143642e425583
Вова Радченко <vettel070897@gmail.com>
2021-01-14 12:06:32 +0200

    chaged text

--- Test project for github/Test project for github/Program.cs
+++ Test project for github/Test project for github/Program.cs
@@ -6,7 +6,7 @@ namespace Test_project_for_github
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! 15");
             int i = 0;
             Console.WriteLine(i);
             Console.WriteLine("Test");
