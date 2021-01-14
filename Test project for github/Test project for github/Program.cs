3e162b10676abebd94034911a1961f0475e9a963
Вова Радченко <vettel070897@gmail.com>
2021-01-14 11:57:04 +0200

    just add some code

---
+++ Test project for github/Test project for github/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_project_for_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
